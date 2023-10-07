ac32df040633d15856b48f64d09c2fa7b07c628c
lhmuana <edd_vai@yahoo.co.in>
2023-10-08 00:23:38 +0530

    simple top app bar is created

--- app/src/main/java/com/example/scaffold_animation_app/MainActivity.kt
+++ app/src/main/java/com/example/scaffold_animation_app/MainActivity.kt
@@ -3,12 +3,22 @@ package com.example.scaffold_animation_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.scaffold_animation_app.ui.theme.Scaffold_Animation_AppTheme
 
@@ -29,12 +39,43 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
+    Scaffold(
+        topBar = { TopAppBar()}
+    ) {
+        contentPadding ->
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.padding(contentPadding)
     )
+}
+}
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(title = {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(dimensionResource(id = R.dimen.image_size))
+                    .padding(dimensionResource(id = R.dimen.padding_small)),
+                painter = painterResource(R.drawable.ic_woof_logo),
+                contentDescription = null
+            )
+
+            Text(
+                text = "Woof",
+                style = MaterialTheme.typography.displayLarge
+            )
+
+
+        }
+    })
 }
 
 @Preview(showBackground = true)
